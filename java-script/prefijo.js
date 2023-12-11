815fdb292dbb861eabe9fd017f46f5f560213858
SantiagoSandoval <sandovalbgasantiago@gmail.com>
2023-12-11 14:51:59 -0500

    ejercicio

---
+++ java-script/prefijo.js
@@ -0,0 +1,8 @@
+const palabras = ["attempt", "pay", "practice", "attend"]
+let prefijo = "pref"
+
+let cadenaPrefijos = palabras.reduce((c, e){
+    if (e.startsWith) {
+        console.log()
+    }
+} )
