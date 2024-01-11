725e3a40ab28f8653e2c21ff5c78f575ff587240
Daniel Andres Tobon Comba <106559688+DanielAndresTobonComba@users.noreply.github.com>
2024-01-11 15:41:53 -0500

    Add files via upload

--- java-script/26-Eventos-bubles/calculadora/calculadora.js
+++ java-script/26-Eventos-bubles/calculadora/calculadora.js
@@ -1,44 +1,15 @@
 
-/* function obtenerValor (){
-  
-    let boton = this
-    let valor = boton.value
-    let valorNumerico = Number(valor)
-    console.log(typeof(valorNumerico))
-    console.log(valorNumerico) 
-
-    let operacion = this 
-    let cual = operacion.value
-
-    
-    arregloNumeros.push(valorNumerico)
-    console.log(arregloNumeros)
-
-    if (arregloNumeros.length > 1){
-
-        if(cual == "suma"){
-            sumar(arregloNumeros)
-
-        }else if (cual == "restar"){
-            restar(arregloNumeros)
-
-        }else if (cual == "multiplicar"){
-            multiplicar(arregloNumeros)
-
-        }else{
-            dividir(arregloNumeros)
-        }
-    }
-    
-    
-} */
-
 let d = document
 let cadena = ""
 //let operador = ""
-let cadenaOperadores = ["+" , "-" , "*" , "/"]
+let cadenaOperadores = ["+" , "-" , "x" , "/"]
+let operacionHecha = false
+
+let contenedor = d.querySelector(".areaOperar")
 
 d.querySelector(".igual").addEventListener("click" , operar)
+
+d.querySelector(".decimal").addEventListener("click" , obtenerValor)
 
 for (let i = 0; i <= 9; i++) {
     d.querySelector(".numero" + i).addEventListener("click", obtenerValor);
@@ -52,16 +23,32 @@ for (let i = 1; i <= 4; i++) {
 //console.log(botones)
 
 function obtenerValor (){
-    let valor = this.textContent
-    cadena += valor
-    console.log(cadena)
+    
+    let valor = this.textContent;
+    console.log("valor entrante: " + valor);
+    
+    if((valor == ".") && (cadena.endsWith("."))){
+        valor = ""
+    }
+
+    if ((cadena.endsWith("+") || cadena.endsWith("-") || cadena.endsWith("x") || cadena.endsWith("/")) && (valor === "+" || valor === "-" || valor === "/" || valor === "x")) {
+        console.log("Ya hay un operador");
+        console.log("Operador: " + valor);
+        valor = "";
+    }  
+
+    // for (let i = 0; i < cadenaOperadores; i++) { }
+
+    cadena += valor;
+    console.log("cadena: " + cadena);
+
 }
 
-function obtenerOperador (boton){
+/* function obtenerOperador (boton){
     let valor = boton.textContent 
     console.log(valor)
     
-}
+} */
 
 
 function operar(){
@@ -73,17 +60,35 @@ function operar(){
 
         if(arregloCadena.length > 1 ){
             
+            operacionHecha = true
+
             console.log(cadenaOperadores[i])
             if(cadenaOperadores[i] == "+"){
                 console.log("es una suma")
+                let suma = Number(arregloCadena[0])  + Number(arregloCadena[1])
+                console.log(suma)
+                cadena = suma.toFixed(2)
+                contenedor.innerHTML = cadena
                 break
             }else if(cadenaOperadores[i] == "-"){
                 console.log("es una resta")
+                let resta = Number(arregloCadena[0])  - Number(arregloCadena[1])
+                console.log(resta)
+                cadena = resta.toFixed(2)
+                contenedor.innerHTML = cadena
                 break
-            }else if (cadenaOperadores[i] == "*"){
+            }else if (cadenaOperadores[i] == "x"){
                 console.log("es un multiplicacion")
-            }else if (cadenaOperadores[i] == "*"){
+                let mult = Number(arregloCadena[0])  * Number(arregloCadena[1])
+                console.log(mult)
+                cadena = mult.toFixed(2)
+                contenedor.innerHTML = cadena
+            }else if (cadenaOperadores[i] == "/"){
                 console.log("es una division")
+                let div = Number(arregloCadena[0])  / Number(arregloCadena[1])
+                console.log(div)
+                cadena = div.toFixed(2)
+                contenedor.innerHTML = cadena
             }else{
                 alert("error")
             }
@@ -94,21 +99,7 @@ function operar(){
 
 
 
-function sumar (){
-    
-}
 
-function restar (){
-    
-}
-
-function multiplicar (){
-    
-}
-
-function dividir (){
-    
-}
 
 
 
